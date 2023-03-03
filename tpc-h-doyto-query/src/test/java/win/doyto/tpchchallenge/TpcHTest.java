7aa0cf7141a11b318f2edc9f259c3b91ee21a408
Forb Yuan <f0rb@163.com>
2023-03-03 17:21:22 +0800

    Q1 - Pricing Summary Report Query

---
+++ tpc-h-doyto-query/src/test/java/win/doyto/tpchchallenge/TpcHTest.java
@@ -0,0 +1,51 @@
+package win.doyto.tpchchallenge;
+
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
+import org.assertj.core.groups.Tuple;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import win.doyto.query.core.DataQueryClient;
+import win.doyto.tpchchallenge.q1.PricingSummaryQuery;
+import win.doyto.tpchchallenge.q1.PricingSummaryView;
+
+import java.math.BigDecimal;
+import java.util.List;
+import javax.annotation.Resource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * TpcHTest
+ *
+ * @author f0rb on 2023/2/23
+ */
+@SpringBootTest(classes = TpcHApplication.class)
+class TpcHTest {
+
+    RecursiveComparisonConfiguration configuration = RecursiveComparisonConfiguration
+            .builder().withComparatorForType(BigDecimal::compareTo, BigDecimal.class).build();
+
+    @Resource
+    private DataQueryClient dataQueryClient;
+
+    @Test
+    void queryForPricingSummaryReport() {
+        PricingSummaryQuery query = PricingSummaryQuery
+                .builder()
+                .shipdateDelta(90)
+                .sort("l_returnflag;l_linestatus")
+                .build();
+
+        List<PricingSummaryView> list = dataQueryClient.aggregate(query, PricingSummaryView.class);
+
+        assertThat(list).hasSize(3);
+        assertThat(list).extracting("l_returnflag", "l_linestatus", "avg_disc")
+                        .usingRecursiveFieldByFieldElementComparator(configuration)
+                        .containsExactly(
+                                Tuple.tuple("A", "F", BigDecimal.valueOf(0.048)),
+                                Tuple.tuple("N", "O", BigDecimal.valueOf(0.051)),
+                                Tuple.tuple("R", "F", BigDecimal.valueOf(0.046666666667))
+                        );
+    }
+
+}
